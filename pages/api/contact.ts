55014e0f2920386273852e0b967b4245da4eefb8
hasaanmumtaz6 <hasaanmumtaz6@gmail.com>
2024-05-27 08:29:35 -0700

    checking changeges

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,26 +1,70 @@
-// pages/api/contact.ts
-import type { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from 'nodemailer';
+// // pages/api/contact.ts
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import nodemailer from 'nodemailer';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === 'POST') {
+//     const { name, email, message } = req.body;
+
+//     // Set up Nodemailer transporter
+//     const transporter = nodemailer.createTransport({
+//         host: "smtp.gmail.com",
+//         port: 587,
+//         secure: false,
+//         auth: {
+//           user: "hasaanmumtaz6@gmail.com",
+//           pass: "wquw dgmc obju skfx",
+//         },
+//       });
+
+//     try {
+//       await transporter.sendMail({
+//         from: email,
+//         to: "hasaanad12min@gmail.com",
+//         subject: `New contact form submission from ${name}`,
+//         text: message,
+//         html: `<p>You have a new contact form submission</p><br>
+//                <p><strong>Name: </strong> ${name} </p><br>
+//                <p><strong>Email: </strong> ${email} </p><br>
+//                <p><strong>Message: </strong> ${message} </p><br>`,
+//       });
+
+//       res.status(200).json({ message: 'Email sent successfully' });
+//     } catch (error) {
+//       console.error('Error sending email', error);
+//       res.status(500).json({ message: 'Error sending email' });
+//     }
+//   } else {
+//     res.setHeader('Allow', ['POST']);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     const { name, email, message } = req.body;
 
     // Set up Nodemailer transporter
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-          user: "hasaanmumtaz6@gmail.com",
-          pass: "wquw dgmc obju skfx",
-        },
-      });
+      host: "smtp.privateemail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "contactus@startupmaxim.com",
+        pass: "Contactus-startupmaxim",
+      },
+    });
 
     try {
       await transporter.sendMail({
         from: email,
-        to: "hasaanad12min@gmail.com",
+        to: "contactus@startupmaxim.com",
         subject: `New contact form submission from ${name}`,
         text: message,
         html: `<p>You have a new contact form submission</p><br>
@@ -29,13 +73,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                <p><strong>Message: </strong> ${message} </p><br>`,
       });
 
-      res.status(200).json({ message: 'Email sent successfully' });
-    } catch (error) {
-      console.error('Error sending email', error);
-      res.status(500).json({ message: 'Error sending email' });
+      res.status(200).json({ message: "Email sent successfully" });
+    } catch (error:any) {
+      console.error("Error sending email", error);
+      res
+        .status(500)
+        .json({ message: "Error sending email", error: error.message });
     }
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
